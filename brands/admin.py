1c193371fadfc2803c4023e19ca281a67d56c74f
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2025-10-21 23:15:05 -0300

    BRAND APP
    Setting templates and others features

--- brands/admin.py
+++ brands/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Brand)
+class BrandsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
