405b11d7ec59b64729707ade25fd964e99f48ea2
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2025-11-11 21:20:22 -0300

    att

--- app/urls.py
+++ app/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # HOME
+    path('', views.home, name='home'),
     # BRANDS URLS
     path('', include('brands.urls')),
     # CATEGORIES URLS
