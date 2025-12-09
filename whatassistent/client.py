2f201ceb35d2e2f7cce68ba3505e1ad71a78f42b
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2025-12-09 16:27:40 -0300

    att

---
+++ whatassistent/client.py
@@ -0,0 +1,19 @@
+import requests
+from decouple import config
+
+
+class CallMeBot:
+    
+    def __init__(self):
+        self.__base_url = config('BASE_URL')
+        self.__phone_number = config('PHONE_NUMBER')
+        self.__api_key = config('API_KEY')
+
+    def send_message(self, message):
+        URL = f'{self.__base_url}phone={self.__phone_number}&text={message}&apikey={self.__api_key}'
+        __response = requests.get(url=URL)
+
+        # data = __response.text
+        # print(__response.text)
+        
+        return __response
