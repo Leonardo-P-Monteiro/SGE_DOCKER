10e02807d9da8c892f1c20f5a07c20695fd598ac
Leonardo P Monteiro <dev.pmonteiro@gmail.com>
2026-01-02 20:42:21 -0300

    STANDARD COMMIT

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$045m1lmzse=pcxjc)4kmb2s(u1mwzqzky&mzh3+da1msiw7a8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
